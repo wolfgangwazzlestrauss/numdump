deaa3b270b65d0d37847f867c555270a4106acdc
Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>
2020-05-21 00:36:35 -0700

    :white_check_mark: Expand test coverage

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wolfgangwazzlestrauss/intdump
 
 go 1.14
+
+require github.com/stretchr/testify v1.5.1
--- test/main_test.go
+++ test/main_test.go
@@ -2,16 +2,19 @@ package morestrings
 
 import (
 	"testing"
+	
+	"github.com/stretchr/testify/assert"
+
 	"github.com/wolfgangwazzlestrauss/intdump/pkg/intdump"
 )
 
 func TestPrintBytes(t *testing.T) {
-	bytes := []byte{1, 2}
+	bytes := []byte{1, 2, 201, 156, 34}
 
-	expected := "0000000   1   2\n"
+	expected := "" +
+		"0000000   1   2 201 156\n" +
+		"0000004  34\n"
 	actual := intdump.DumpBytes(bytes, 4)
 
-	if actual != expected {
-		t.Errorf("\nactual = %s;\nexpected = %s", actual, expected)
-	}
+	assert.Equal(t, expected, actual)
 }
